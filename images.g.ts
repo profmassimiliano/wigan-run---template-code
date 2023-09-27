2c647949f0e78d487724dcb612dd6c917d4e644d
ProfMax <46679107+profmassimiliano@users.noreply.github.com>
2023-09-27 19:15:49 +0200

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,82 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image2":
+            case "merit":return img`
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 f f 1 1 1 1 1 1 1 f 1 1 1 1 
+1 1 f f f 1 1 1 1 f f f 1 1 1 1 
+1 1 f 1 f f 1 1 f f 1 f 1 1 1 1 
+1 1 f 1 1 f 1 1 f 1 1 f 1 1 1 1 
+1 1 f 1 1 1 f f f 1 1 f 1 1 1 1 
+1 1 f 1 1 1 1 1 1 1 1 f 1 1 1 1 
+1 1 f 1 1 1 1 1 1 1 1 f 1 1 1 1 
+1 1 f 1 1 1 1 1 1 1 1 f 1 1 1 1 
+1 1 f 1 1 1 1 1 1 1 1 f 1 1 1 1 
+1 1 f 1 1 1 1 1 1 1 1 f 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+`;
+            case "image1":
+            case "cow":return img`
+. . . . . . . . . . f . . f . . 
+. . . . . . . . . . f f f f . . 
+. . . . . . . . . . 1 f f 1 . . 
+. . . . . . . . . . f f f f . . 
+. . f f f f f f f f f f . . . . 
+. . f f f f f f f f f f . . . . 
+. . f f f f f f f 1 1 1 . . . . 
+. . f f 1 1 1 f f 1 1 1 . . . . 
+. . f f 1 1 1 f f f f f . . . . 
+. . f f 1 1 1 f f f f f . . . . 
+. . f f f f f f f f f f . . . . 
+. . f f f f f f f f f f . . . . 
+. . f f f f f f f f f f . . . . 
+. . f f f f f f f f f f . . . . 
+. . f f . . . . . f f . . . . . 
+. . f f . . . . . f f . . . . . 
+`;
+            case "image3":
+            case "teacher":return img`
+. . . . . 3 3 3 3 3 . . . . . . 
+. . . . . 3 3 3 3 3 . . . . . . 
+. . . . . 3 3 3 3 3 . . . . . . 
+. . 9 9 9 9 9 9 9 9 9 9 9 . . . 
+. . 9 . . 9 9 9 9 9 . . 9 . . . 
+. . 9 . . 9 9 9 9 9 . . 9 . . . 
+. . . . . 9 9 9 9 9 . . . . . . 
+. . . . . 9 9 9 9 9 . . . . . . 
+. . . . . e e e e e . . . . . . 
+. . . . . e e . e e . . . . . . 
+. . . . . e e . e e . . . . . . 
+. . . . . e e . e e . . . . . . 
+. . . . . e e . e e . . . . . . 
+. . . . . e e . e e . . . . . . 
+. . . f f f f . f f f f . . . . 
+. . . f f f f . f f f f . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
